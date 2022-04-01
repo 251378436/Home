f9daa3a636795c96203fa5ffcba8adb7af0a3b9f
251378436 <251378436@qq.com>
2022-04-01 19:46:45 +1300

    Add new apiurl environment variable

--- Home.Client/src/app/app.component.ts
+++ Home.Client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'oidc-client';
+  apiUrl: any;
+
+  constructor(){
+    console.log(environment);
+    this.apiUrl = environment.apiUrl;
+  }
 }
