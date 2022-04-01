867c6fc35d6e8b01aa69c2a27c493c085fdfac9a
251378436 <251378436@qq.com>
2022-04-02 10:00:46 +1300

    Add test environment

--- Home.Client/src/environments/environment.prod.ts
+++ Home.Client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://baidu.com"
+  apiUrl: "https://prod.com"
 };
--- Home.Client/src/environments/environment.ts
+++ Home.Client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://google.com"
+  apiUrl: "https://local.com"
 };
 
 /*
